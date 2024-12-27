4c30382269a24d6c8db3943e5461ac5752105936
Jui Kalan <juikalan21@gmail.com>
2024-12-27 18:22:25 +0530

    first commit

---
+++ catMeIfYouCan.js
@@ -0,0 +1,25 @@
+let score = 0;
+const cat = document.getElementById('cat');
+const scoreDisplay = document.getElementById('score');
+const holes = document.querySelectorAll('.hole');
+
+function getRandomHole(){
+    const randomIndex = Math.floor(Math.random()*holes.length);
+    return holes[randomIndex];
+}
+function showCat() {
+    const hole = getRandomHole();
+    cat.style.left = (hole.offsetLeft + 18) + 'px';
+    cat.style.bottom = (hole.offsetTop - 200) + 'px';
+    cat.style.display = 'block' ;
+}
+
+
+cat.addEventListener('click' , () =>{
+    score++;
+    scoreDisplay.textContent = 'Score: ' + score ;
+    cat.style.display = 'none' ;
+    setTimeout(showCat,500);
+})
+
+setInterval(showCat, 1000);
